14de288b07b8815dda766de42fe9807cb978b3a4
punwai <punwai@yahoo.com>
2023-11-17 07:49:51 -0800

    save

--- tune/finetune.py
+++ tune/finetune.py
@@ -1,8 +1,10 @@
 # Training Harness for the diffusion model
 
 import argparse
-
 from diffusionjockey.utils import audio_to_spectrogram
+import os
+import subprocess
+import PIL.Image as Image
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -11,9 +13,17 @@ def read_args():
 
 args = read_args()
 
-import PIL.Image as Image
 def finetune():
     print("hallo")
     spectogram, sampling_rate = audio_to_spectrogram("./examples/audio.mp3")
     print(spectogram)
     spectogram[0].show()
+
+    image_folder = "./spectogram_images"
+    os.makedirs(image_folder, exist_ok=True)
+    spectogram[0].save(os.path.join(image_folder, "img_0.png"))
+
+    subprocess.call([
+        "python",
+        "tune/train_text_to_image_lora_sdxl.py"
+    ])
