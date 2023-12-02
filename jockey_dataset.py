0ca4ebc904e2f8c2475799f288f5458eca4070ae
bryan chiang <bryanhpchiang@gmail.com>
2023-12-02 23:01:36 +0000

    update diffusion jockey dataset

--- jockey_dataset.py
+++ jockey_dataset.py
@@ -1,37 +1,46 @@
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 from datasets.features.image import Image
 import torchvision
 import torch
+import os
 
 def load_custom_dataset():
-    # Directory for training inputs
-    WRITE_DIR = 'input/'
-    AUDIO_DIR = 'fma/data/fma_small/'
+    dataset = load_dataset("punwaiw/DiffusionJockey")
+    return dataset["train"]
 
-    # Turn it into huggingface dataset format
 
-    descriptions = torch.load(WRITE_DIR + 'descriptions.pt')
+# def load_custom_dataset():
+#     # Directory for training inputs
+#     WRITE_DIR = 'input/'
+#
+#     # Turn it into huggingface dataset format
+#
+#     descriptions = torch.load(WRITE_DIR + 'descriptions.pt')
+#
+#     image_list, text_list = [], []
+#
+#     print(descriptions)
+#
+#     for folder in os.listdir(WRITE_DIR):
+#         print(f"LOADING FOLDER {folder}")
+#         item_path = os.path.join(WRITE_DIR, folder)
+#
+#         if os.path.isdir(item_path):
+#             for filename in os.listdir(item_path):
+#                 spectrogram_path = os.path.join(item_path, filename)
+#                 spectrogram = torch.load(spectrogram_path)[0]
+#                 spectrogram = spectrogram.unsqueeze(0)
+#                 spectrogram = spectrogram.expand(3, -1, -1)
+#
+#                 track_id = int(filename[:-3])
+#                 text = descriptions[track_id]
+#                 image = torchvision.transforms.ToPILImage()(spectrogram)
+#                 image_list.append(image)
+#                 text_list.append(text)
+#
+#     image_data = Dataset.from_dict({"image": image_list, "text": text_list})
+#     return image_data
 
-    print(descriptions)
-
-    image_list, text_list = [], []
-
-    for track_id in descriptions:
-        spectrogram_path = f"{WRITE_DIR}{track_id}.pt"
-        spectrogram = torch.load(spectrogram_path)[0]
-        spectrogram = spectrogram.unsqueeze(0)
-        spectrogram = spectrogram.expand(3, -1, -1)
-        text = descriptions[track_id]
-
-        print(spectrogram.shape)
-        image = torchvision.transforms.ToPILImage()(spectrogram)
-
-        image_list.append(image)
-        text_list.append(text)
-
-    image_data = Dataset.from_dict({"image": image_list, "text": text_list})
-    return image_data
-
-# dataset = load_custom_dataset()
-# dataset.push_to_hub("punwaiw/DiffusionJockey")
+dataset = load_custom_dataset()
+print(dataset)
 
